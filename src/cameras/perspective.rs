c923d9071685ff1d8e37652c4b0f5f439b8dd9fd
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-28 19:18:25 +0200

    perspective camera

--- src/cameras/perspective.rs
+++ src/cameras/perspective.rs
@@ -1,4 +1,4 @@
-use crate::math::{rectangle3d::Rectangle3D, point3d::{Point3D, p3}, vector3d::{Vector3D, v3}};
+use crate::math::{ray::Ray, rectangle3d::Rectangle3D, point3d::{Point3D, p3}, vector3d::{Vector3D, v3}, point2d::Point2D};
 
 
 pub struct PerspectiveCamera {
@@ -13,6 +13,12 @@ pub struct PerspectiveCameraParameters {
     aspect_ratio: f64,
 }
 
+pub struct Rays<'a> {
+    parent: &'a PerspectiveCamera,
+    point: Point2D,
+    consumed: bool,
+}
+
 impl PerspectiveCamera {
     pub fn new(parameters: &PerspectiveCameraParameters) -> PerspectiveCamera {
         let screen_width = parameters.aspect_ratio;
@@ -23,5 +29,28 @@ impl PerspectiveCamera {
         let screen = Rectangle3D::new(origin, x_axis, y_axis);
 
         PerspectiveCamera { screen }
+    }
+
+    pub fn enumerate_rays(&self, point: Point2D) -> Rays {
+        Rays { parent: self, point, consumed: false }
+    }
+}
+
+impl<'a> Iterator for Rays<'a> {
+    type Item = Ray;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.consumed {
+            None
+        } else {
+            self.consumed = true;
+
+            let origin = p3!(0, 0, 0);
+            let to = self.parent.screen.from_relative(self.point);
+            let direction = to - origin;
+            let ray = Ray::new(origin, direction);
+
+            Some(ray)
+        }
     }
 }
