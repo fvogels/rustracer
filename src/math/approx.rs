4f6a45767e08875c89debad39cf2ce6c5dc3a367
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-28 23:29:52 +0200

    added tests to approx

--- src/math/approx.rs
+++ src/math/approx.rs
@@ -10,16 +10,38 @@ pub trait Approx {
 
 impl<T : Metric> Approx for T {
     fn approx_eps(&self, rhs: &Self, epsilon: f64) -> bool {
-        rhs.distance(rhs) < epsilon
+        self.distance(rhs) < epsilon
     }
 }
 
-struct ApproxWrapper<T : Approx> {
+pub struct ApproxWrapper<T : Approx> {
     value: T,
+}
+
+pub fn approx<T : Approx>(value : T) -> ApproxWrapper<T> {
+    ApproxWrapper { value }
 }
 
 impl<T : Approx> PartialEq<T> for ApproxWrapper<T> {
     fn eq(&self, other: &T) -> bool {
         self.value.approx(other)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[cfg(test)]
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(0.0, 0.0, true)]
+    #[case(0.0, 0.0000001, true)]
+    #[case(8.0, 7.9999999, true)]
+    #[case(8.0, 8.0000001, true)]
+    #[case(0.0, 1.0, false)]
+    fn approx_f64(#[case] x: f64, #[case] y: f64, #[case] expected: bool) {
+        assert_eq!(expected, approx(x) == y);
+        assert_eq!(expected, approx(y) == x);
     }
 }
