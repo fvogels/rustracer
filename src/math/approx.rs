7fc84686b90cfb99451891253def4aa7f10ffbb1
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-28 22:52:52 +0200

    ApproxWrapper

--- src/math/approx.rs
+++ src/math/approx.rs
@@ -13,3 +13,13 @@ impl<T : Metric> Approx for T {
         rhs.distance(rhs) < epsilon
     }
 }
+
+struct ApproxWrapper<T : Approx> {
+    value: T,
+}
+
+impl<T : Approx> PartialEq<T> for ApproxWrapper<T> {
+    fn eq(&self, other: &T) -> bool {
+        self.value.approx(other)
+    }
+}
