54dc77b79e187490ed414b823c8b01d65309c474
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-29 16:50:07 +0200

    Hit::closest

--- src/primitives/primitive.rs
+++ src/primitives/primitive.rs
@@ -16,3 +16,12 @@ pub struct HitPosition {
 pub trait Primitive {
     fn find_first_positive_hit(&self, ray: &Ray) -> Option<Hit>;
 }
+
+impl Hit {
+    pub fn closest(hit1: Hit, hit2: Hit) -> Hit {
+        debug_assert!(hit1.t >= 0.0);
+        debug_assert!(hit2.t >= 0.0);
+
+        if hit1.t < hit2.t { hit1 } else { hit2 }
+    }
+}
