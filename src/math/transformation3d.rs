2eab6e62389ae9bb084889143f97f8b193b8593e
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-29 16:08:28 +0200

    rotation transformations

--- src/math/transformation3d.rs
+++ src/math/transformation3d.rs
@@ -1,4 +1,4 @@
-use super::{matrix4d::Matrix4D, vector3d::Vector3D};
+use super::{matrix4d::Matrix4D, vector3d::Vector3D, angle::Angle};
 
 pub struct Transformation3D {
     pub matrix: Matrix4D,
@@ -16,6 +16,27 @@ impl Transformation3D {
     pub fn scale(sx: f64, sy: f64, sz: f64) -> Transformation3D {
         let matrix = Matrix4D::scale(sx, sy, sz);
         let inverse_matrix = Matrix4D::scale(sx.recip(), sy.recip(), sz.recip());
+
+        Transformation3D { matrix, inverse_matrix }
+    }
+
+    pub fn rotate_around_x(angle: Angle) -> Transformation3D {
+        let matrix = Matrix4D::rotate_around_x(angle);
+        let inverse_matrix = Matrix4D::rotate_around_x(-angle);
+
+        Transformation3D { matrix, inverse_matrix }
+    }
+
+    pub fn rotate_around_y(angle: Angle) -> Transformation3D {
+        let matrix = Matrix4D::rotate_around_y(angle);
+        let inverse_matrix = Matrix4D::rotate_around_y(-angle);
+
+        Transformation3D { matrix, inverse_matrix }
+    }
+
+    pub fn rotate_around_z(angle: Angle) -> Transformation3D {
+        let matrix = Matrix4D::rotate_around_z(angle);
+        let inverse_matrix = Matrix4D::rotate_around_z(-angle);
 
         Transformation3D { matrix, inverse_matrix }
     }
@@ -51,6 +72,27 @@ mod tests {
     #[case(3.0, 4.0, 5.0)]
     fn scale_inverse_matrix(#[case] sx: f64, #[case] sy: f64, #[case] sz: f64) {
         let transformation = Transformation3D::scale(sx, sy, sz);
+
+        assert_eq!(approx(Matrix4D::identity()), &transformation.matrix * &transformation.inverse_matrix);
+    }
+
+    #[rstest]
+    fn rotate_x_inverse_matrix(#[values(0.0, 90.0, 180.0, 45.0, 60.0, 135.0)] degrees: f64) {
+        let transformation = Transformation3D::rotate_around_x(Angle::degrees(degrees));
+
+        assert_eq!(approx(Matrix4D::identity()), &transformation.matrix * &transformation.inverse_matrix);
+    }
+
+    #[rstest]
+    fn rotate_y_inverse_matrix(#[values(0.0, 90.0, 180.0, 45.0, 60.0, 135.0)] degrees: f64) {
+        let transformation = Transformation3D::rotate_around_y(Angle::degrees(degrees));
+
+        assert_eq!(approx(Matrix4D::identity()), &transformation.matrix * &transformation.inverse_matrix);
+    }
+
+    #[rstest]
+    fn rotate_z_inverse_matrix(#[values(0.0, 90.0, 180.0, 45.0, 60.0, 135.0)] degrees: f64) {
+        let transformation = Transformation3D::rotate_around_z(Angle::degrees(degrees));
 
         assert_eq!(approx(Matrix4D::identity()), &transformation.matrix * &transformation.inverse_matrix);
     }
