3f564749465b9ffe37e58bde23bf85d6a2fb0cb2
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-30 00:12:11 +0200

    &Color * f64

--- src/imaging/color.rs
+++ src/imaging/color.rs
@@ -96,6 +96,18 @@ impl std::ops::Mul<f64> for Color {
     }
 }
 
+impl std::ops::Mul<f64> for &Color {
+    type Output = Color;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let r = self.r() * rhs;
+        let g = self.g() * rhs;
+        let b = self.b() * rhs;
+
+        Color::new(r, g, b)
+    }
+}
+
 impl std::ops::Div<f64> for Color {
     type Output = Self;
 
