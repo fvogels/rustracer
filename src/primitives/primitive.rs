c15f16042223e6de3d34377236f1241863ca632d
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-29 23:16:37 +0200

    updated method of picking best hit

--- src/primitives/primitive.rs
+++ src/primitives/primitive.rs
@@ -1,10 +1,12 @@
-use crate::math::{point3d::Point3D, ray::Ray, vector3d::Vector3D, point2d::Point2D};
+use std::rc::Rc;
 
-#[derive(Copy, Clone)]
+use crate::{math::{point3d::Point3D, ray::Ray, vector3d::Vector3D, point2d::Point2D}, materials::material::Material};
+
 pub struct Hit {
     pub t: f64,
     pub position: HitPosition,
     pub normal: Vector3D,
+    pub material: Option<Rc<dyn Material>>,
 }
 
 #[derive(Copy, Clone)]
@@ -24,12 +26,26 @@ pub trait Primitive {
 }
 
 impl Hit {
-    pub fn overwrite_if_closer(&mut self, hit: &Hit) {
-        debug_assert!(self.t >= 0.0);
-        debug_assert!(hit.t >= 0.0);
-
-        if hit.t < self.t {
-            *self = *hit;
+    pub fn smallest_positive(hit1: Hit, hit2: Hit) -> Option<Hit> {
+        if hit1.t < 0.0 {
+            if hit2.t < 0.0 {
+                debug_assert!(hit1.t < 0.0 && hit2.t < 0.0);
+                None
+            } else {
+                debug_assert!(hit1.t < 0.0 && hit2.t >= 0.0);
+                Some(hit2)
+            }
+        }
+        else if hit2.t < 0.0 {
+            debug_assert!(hit1.t >= 0.0 && hit2.t < 0.0);
+            Some(hit1)
+        } else {
+            debug_assert!(hit1.t >= 0.0 && hit2.t >= 0.0);
+            if hit1.t < hit2.t {
+                Some(hit1)
+            } else {
+                Some(hit2)
+            }
         }
     }
 }
