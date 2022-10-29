f8996c8ab8a35bfad6207fe06dc44d5077e848c9
Frederic Vogels <frederic.vogels@ucll.be>
2022-10-29 03:15:43 +0200

    Rasterizer2D lifetime & cleanup

--- src/math/rasterizer2d.rs
+++ src/math/rasterizer2d.rs
@@ -1,14 +1,14 @@
 use super::position2d::Position2D;
 use super::rectangle2d::Rectangle2D;
 
-pub struct Rasterizer2D {
-    rectangle: Rectangle2D,
+pub struct Rasterizer2D<'a> {
+    rectangle: &'a Rectangle2D,
     width: u32,
     height: u32,
 }
 
-impl Rasterizer2D {
-    pub fn new(rectangle: Rectangle2D, width: u32, height: u32) -> Rasterizer2D {
+impl<'a> Rasterizer2D<'a> {
+    pub fn new(rectangle: &'a Rectangle2D, width: u32, height: u32) -> Rasterizer2D {
         Rasterizer2D {
             rectangle,
             width,
@@ -20,8 +20,8 @@ impl Rasterizer2D {
         assert!(0 <= position.x && position.x < self.width as i32);
         assert!(0 <= position.y && position.y < self.height as i32);
 
-        let x_axis = self.rectangle.x_axis * (1.0 / (self.width as f64));
-        let y_axis = self.rectangle.y_axis * (1.0 / (self.width as f64));
+        let x_axis = self.rectangle.x_axis * (self.width as f64).recip();
+        let y_axis = self.rectangle.y_axis * (self.width as f64).recip();
         let origin =
             self.rectangle.origin + x_axis * (position.x as f64) + y_axis * (position.y as f64);
 
