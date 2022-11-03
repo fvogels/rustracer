2d039e6bb186fad3186866fbbeaed3fcc78de027
Frederic Vogels <frederic.vogels@ucll.be>
2022-11-03 23:35:03 +0100

    graph

--- src/data/graph.rs
+++ src/data/graph.rs
@@ -1,87 +1,130 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
-
-pub struct Graph<N, A> {
-    nodes: Vec<Node<N, A>>,
+pub struct Graph<VertexLabel, EdgeLabel> {
+    vertices: Vec<Vertex<VertexLabel, EdgeLabel>>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+pub struct Vertex<VertexLabel, EdgeLabel> {
+    label: VertexLabel,
+    departing_edges: HashMap<VertexId, Vec<EdgeLabel>>,
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Copy, Clone)]
+pub struct VertexId {
+    index: usize,
+}
+
+#[derive(Debug)]
 pub enum Error {
-    InvalidNode,
-    NoArcs,
+    InvalidVertexId(VertexId),
+    NoArcsTo(VertexId),
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub struct NodeId {
-    id: usize,
-}
-
-struct Node<N, A> {
-    label: N,
-    arcs: HashMap<usize, Vec<A>>,
-}
-
-impl<N, A> Graph<N, A> {
-    pub fn new() -> Graph<N, A> {
+impl<VertexLabel, EdgeLabel> Graph<VertexLabel, EdgeLabel> {
+    pub fn new() -> Graph<VertexLabel, EdgeLabel> {
         Graph {
-            nodes: Vec::new(),
+            vertices: Vec::new(),
         }
     }
 
-    pub fn create_node(&mut self, label: N) -> NodeId {
-        let id = self.nodes.len();
-        let node = Node {
-            arcs: HashMap::new(),
-            label
+    pub fn create_vertex(&mut self, label: VertexLabel) -> VertexId {
+        let index = self.vertices.len();
+        let vertex = Vertex {
+            label,
+            departing_edges: HashMap::new(),
         };
+        self.vertices.push(vertex);
 
-        self.nodes.push(node);
-        NodeId { id }
+        VertexId { index }
     }
 
-    pub fn link(&mut self, from: NodeId, to: NodeId, label: A) -> Result<(), Error> {
-        let mut node = self.nodes.get_mut(from.id).ok_or(Error::InvalidNode)?;
-        let mut arcs = &mut node.arcs;
-        let mut labels = arcs.entry(to.id).or_insert_with(|| Vec::new());
-        labels.push(label);
-        Ok(())
+    pub fn vertex_label(&self, vertex: VertexId) -> Result<&VertexLabel, Error> {
+        self.get_vertex(vertex).map(|v| &v.label)
     }
 
-    pub fn node_label(&self, node: NodeId) -> Result<&N, Error> {
-        Ok(&self.get_node(node)?.label)
+    pub fn vertex_label_mut(&mut self, vertex: VertexId) -> Result<&mut VertexLabel, Error> {
+        self.get_vertex_mut(vertex).map(|v| &mut v.label)
     }
 
-    pub fn node_label_mut(&mut self, node: NodeId) -> Result<&mut N, Error> {
-        Ok(&mut self.get_node_mut(node)?.label)
+    pub fn create_edge(
+        &mut self,
+        start: VertexId,
+        end: VertexId,
+        label: EdgeLabel,
+    ) -> Result<(), Error> {
+        let start = self.get_vertex_mut(start)?;
+        start.add_edge_to(end, label)
     }
 
-    pub fn node_count(&self) -> usize {
-        self.nodes.len()
+    pub fn get_vertex(&self, id: VertexId) -> Result<&Vertex<VertexLabel, EdgeLabel>, Error> {
+        self.vertices
+            .get(id.index)
+            .ok_or(Error::InvalidVertexId(id))
     }
 
-    pub fn reachable_from(&self, from: NodeId) -> Result<Vec<NodeId>, Error> {
-        self.get_node(from).map(|n| n.arcs.keys().map(|id| NodeId { id: *id }).collect())
+    pub fn get_vertex_mut(
+        &mut self,
+        id: VertexId,
+    ) -> Result<&mut Vertex<VertexLabel, EdgeLabel>, Error> {
+        self.vertices
+            .get_mut(id.index)
+            .ok_or(Error::InvalidVertexId(id))
     }
 
-    pub fn arcs_between(&self, from: NodeId, to: NodeId) -> Result<&Vec<A>, Error> {
-        self.get_node(from).and_then(|n| n.arcs.get(&to.id).ok_or(Error::NoArcs))
+    pub fn reachable_from(&self, id: VertexId) -> Result<Vec<VertexId>, Error> {
+        self.get_vertex(id).map(|v| v.reachable_from())
     }
 
-    fn get_node(&self, id: NodeId) -> Result<&Node<N, A>, Error> {
-        self.nodes.get(id.id).ok_or(Error::InvalidNode)
+    pub fn arcs_between(&self, from: VertexId, to: VertexId) -> Result<&Vec<EdgeLabel>, Error> {
+        self.get_vertex(from).and_then(|v| v.arcs_to(to))
     }
 
-    fn get_node_mut(&mut self, id: NodeId) -> Result<&mut Node<N, A>, Error> {
-        self.nodes.get_mut(id.id).ok_or(Error::InvalidNode)
+    pub fn vertex_count(&self) -> usize {
+        self.vertices.len()
     }
 }
 
-impl<N, A: PartialEq> Graph<N, A> {
-    pub fn reachable_by(&self, node: NodeId, arc_label: &A) -> Result<Vec<NodeId>, Error> {
-        let reachable = self.reachable_from(node)?;
-        let result = reachable.iter().filter(|&&to| self.arcs_between(node, to).unwrap().contains(arc_label)).map(|&n| n).collect();
+impl<VertexLabel, EdgeLabel: PartialEq> Graph<VertexLabel, EdgeLabel> {
+    pub fn reachable_through(
+        &self,
+        id: VertexId,
+        label: &EdgeLabel,
+    ) -> Result<Vec<VertexId>, Error> {
+        self.get_vertex(id).map(|v| v.reachable_through(label))
+    }
+}
 
-        Ok(result)
+impl<VertexLabel, EdgeLabel> Vertex<VertexLabel, EdgeLabel> {
+    fn add_edge_to(&mut self, end: VertexId, label: EdgeLabel) -> Result<(), Error> {
+        let vector = self
+            .departing_edges
+            .entry(end)
+            .or_insert_with(|| Vec::new());
+        vector.push(label);
+        Ok(())
+    }
+
+    fn reachable_from(&self) -> Vec<VertexId> {
+        self.departing_edges.keys().copied().collect()
+    }
+
+    fn arcs_to(&self, to: VertexId) -> Result<&Vec<EdgeLabel>, Error> {
+        self.departing_edges.get(&to).ok_or(Error::NoArcsTo(to))
+    }
+}
+
+impl<VertexLabel, EdgeLabel: PartialEq> Vertex<VertexLabel, EdgeLabel> {
+    fn reachable_through(&self, label: &EdgeLabel) -> Vec<VertexId> {
+        self.departing_edges
+            .iter()
+            .filter_map(|(&id, edges)| {
+                if edges.contains(&label) {
+                    Some(id)
+                } else {
+                    None
+                }
+            })
+            .collect()
     }
 }
 
@@ -94,119 +137,76 @@ mod tests {
     #[cfg(test)]
     use super::*;
 
-    #[test]
-    fn node_count() {
-        let mut graph: Graph<(), ()> = Graph::new();
+    #[rstest]
+    fn vertex_count() {
+        let mut graph: Graph<i32, ()> = Graph::new();
+        assert_eq!(0, graph.vertex_count());
 
-        let _node = graph.create_node(());
-        assert_eq!(1, graph.node_count());
+        graph.create_vertex(1);
+        assert_eq!(1, graph.vertex_count());
 
-        let _node = graph.create_node(());
-        assert_eq!(2, graph.node_count());
+        graph.create_vertex(21);
+        assert_eq!(2, graph.vertex_count());
 
-        let _node = graph.create_node(());
-        assert_eq!(3, graph.node_count());
+        graph.create_vertex(1);
+        assert_eq!(3, graph.vertex_count());
     }
 
-    #[test]
+    #[rstest]
+    fn vertex_label() {
+        let mut graph: Graph<i32, ()> = Graph::new();
+
+        let v1 = graph.create_vertex(1);
+        let v2 = graph.create_vertex(2);
+        let v3 = graph.create_vertex(3);
+
+        assert_eq!(1, *graph.vertex_label(v1).unwrap());
+        assert_eq!(2, *graph.vertex_label(v2).unwrap());
+        assert_eq!(3, *graph.vertex_label(v3).unwrap());
+    }
+
+    #[rstest]
     fn reachable_from() {
-        let mut graph: Graph<(), ()> = Graph::new();
+        let mut graph: Graph<i32, ()> = Graph::new();
 
-        let n1 = graph.create_node(());
-        let n2 = graph.create_node(());
-        let n3 = graph.create_node(());
+        let v1 = graph.create_vertex(1);
+        let v2 = graph.create_vertex(2);
+        let v3 = graph.create_vertex(3);
+        let v4 = graph.create_vertex(4);
 
-        assert_same_elements!(vec![], graph.reachable_from(n1).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n2).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n3).unwrap());
+        graph.create_edge(v1, v2, ());
+        graph.create_edge(v1, v3, ());
+        graph.create_edge(v1, v4, ());
+        graph.create_edge(v3, v3, ());
+        graph.create_edge(v4, v1, ());
+        graph.create_edge(v4, v2, ());
 
-        graph.link(n1, n2, ()).unwrap();
-        assert_same_elements!(vec![n2], graph.reachable_from(n1).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n2).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n3).unwrap());
-
-        graph.link(n1, n3, ()).unwrap();
-        assert_same_elements!(vec![n2, n3], graph.reachable_from(n1).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n2).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n3).unwrap());
-
-        graph.link(n2, n3, ()).unwrap();
-        assert_same_elements!(vec![n2, n3], graph.reachable_from(n1).unwrap());
-        assert_same_elements!(vec![n3], graph.reachable_from(n2).unwrap());
-        assert_same_elements!(vec![], graph.reachable_from(n3).unwrap());
-
-        graph.link(n3, n1, ()).unwrap();
-        assert_same_elements!(vec![n2, n3], graph.reachable_from(n1).unwrap());
-        assert_same_elements!(vec![n3], graph.reachable_from(n2).unwrap());
-        assert_same_elements!(vec![n1], graph.reachable_from(n3).unwrap());
+        assert_same_elements!(vec![v2, v3, v4], graph.reachable_from(v1).unwrap());
+        assert_same_elements!(vec![], graph.reachable_from(v2).unwrap());
+        assert_same_elements!(vec![v3], graph.reachable_from(v3).unwrap());
+        assert_same_elements!(vec![v1, v2], graph.reachable_from(v4).unwrap());
     }
 
-    #[test]
+    #[rstest]
     fn reachable_by() {
-        let mut graph: Graph<(), i32> = Graph::new();
+        let mut graph: Graph<i32, char> = Graph::new();
 
-        let n1 = graph.create_node(());
-        let n2 = graph.create_node(());
-        let n3 = graph.create_node(());
-        let n4 = graph.create_node(());
+        let v1 = graph.create_vertex(1);
+        let v2 = graph.create_vertex(2);
+        let v3 = graph.create_vertex(3);
+        let v4 = graph.create_vertex(4);
 
-        graph.link(n1, n2, 1).expect("bug");
-        graph.link(n1, n3, 1).expect("bug");
-        graph.link(n1, n3, 2).expect("bug");
-        graph.link(n1, n4, 2).expect("bug");
-        graph.link(n1, n1, 3).expect("bug");
+        graph.create_edge(v1, v1, 'a');
+        graph.create_edge(v1, v2, 'a');
+        graph.create_edge(v1, v2, 'b');
+        graph.create_edge(v1, v3, 'b');
+        graph.create_edge(v1, v2, 'c');
+        graph.create_edge(v1, v3, 'c');
+        graph.create_edge(v1, v4, 'c');
 
-        assert_same_elements!(vec![n2, n3], graph.reachable_by(n1, &1).unwrap());
-        assert_same_elements!(vec![n3, n4], graph.reachable_by(n1, &2).unwrap());
-        assert_same_elements!(vec![n1], graph.reachable_by(n1, &3).unwrap());
-        assert_same_elements!(vec![], graph.reachable_by(n1, &4).unwrap());
-    }
-
-    #[test]
-    fn arcs_between() {
-        let mut graph: Graph<(), i32> = Graph::new();
-
-        let n1 = graph.create_node(());
-        let n2 = graph.create_node(());
-        let n3 = graph.create_node(());
-
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n1, n2));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n1, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n2));
-
-        graph.link(n1, n2, 1).unwrap();
-        assert_same_elements!(vec![1], graph.arcs_between(n1, n2).unwrap());
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n1, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n2));
-
-        graph.link(n1, n3, 2).unwrap();
-        assert_same_elements!(vec![1], graph.arcs_between(n1, n2).unwrap());
-        assert_same_elements!(vec![2], graph.arcs_between(n1, n3).unwrap());
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n2));
-
-        graph.link(n1, n3, 3).unwrap();
-        assert_same_elements!(vec![1], graph.arcs_between(n1, n2).unwrap());
-        assert_same_elements!(vec![2, 3], graph.arcs_between(n1, n3).unwrap());
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n1));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n2));
-
-        graph.link(n3, n1, 4).unwrap();
-        assert_same_elements!(vec![1], graph.arcs_between(n1, n2).unwrap());
-        assert_same_elements!(vec![2, 3], graph.arcs_between(n1, n3).unwrap());
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n3));
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n2, n1));
-        assert_same_elements!(vec![4], graph.arcs_between(n3, n1).unwrap());
-        assert_eq!(Err(Error::NoArcs), graph.arcs_between(n3, n2));
+        assert_same_elements!(vec![v1, v2], graph.reachable_through(v1, &'a').unwrap());
+        assert_same_elements!(vec![v2, v3], graph.reachable_through(v1, &'b').unwrap());
+        assert_same_elements!(vec![v2, v3], graph.reachable_through(v1, &'b').unwrap());
+        assert_same_elements!(vec![v2, v3, v4], graph.reachable_through(v1, &'c').unwrap());
     }
 }
