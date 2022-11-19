ff3e52c1512a996002a75b2f0ef49ef1ea50faa5
Frederic Vogels <frederic.vogels@ucll.be>
2022-11-19 14:00:49 +0100

    Value type predicates

--- src/scripting/values.rs
+++ src/scripting/values.rs
@@ -13,3 +13,54 @@ pub enum Value {
 }
 
 pub type NativeFunction = Rc<dyn Fn(&mut Interpreter, &[Rc<Value>]) -> Result<Rc<Value>, InterpreterError>>;
+
+impl Value {
+    pub fn is_integer(&self) -> bool {
+        match self {
+            Value::Integer(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_float(&self) -> bool {
+        match self {
+            Value::FloatingPointNumber(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_bool(&self) -> bool {
+        match self {
+            Value::Boolean(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_list(&self) -> bool {
+        match self {
+            Value::List(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_symbol(&self) -> bool {
+        match self {
+            Value::Symbol(_) => true,
+            _ => false
+        }
+    }
+
+    pub fn is_nil(&self) -> bool {
+        match self {
+            Value::Nil => true,
+            _ => false
+        }
+    }
+
+    pub fn is_native_function(&self) -> bool {
+        match self {
+            Value::NativeFunction(_) => true,
+            _ => false
+        }
+    }
+}
