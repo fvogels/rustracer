df5ea1fd4bee29dcc6adcc3317f6ab1eefd8c35d
Frederic Vogels <frederic.vogels@ucll.be>
2023-01-14 17:44:04 +0100

    fixed bugs in plane

--- src/primitives/plane.rs
+++ src/primitives/plane.rs
@@ -15,10 +15,10 @@ fn compute_uv_coordinates(p: Point<3>) -> Point<2> {
     pt!(p.x(), p.y())
 }
 
-fn compute_coordinate_system(origin: Point<3>) -> CoordinateSystem3D {
+fn compute_coordinate_system(origin: Point<3>, ray_origin: &Point<3>) -> CoordinateSystem3D {
     let x_axis = vc!(1, 0, 0);
     let y_axis = vc!(0, 1, 0);
-    let z_axis = vc!(0, 0, 1);
+    let z_axis = if ray_origin.z() > 0.0 { vc!(0, 0, 1) } else { vc!(0, 0, -1) };
 
     CoordinateSystem3D {
         origin,
@@ -36,21 +36,64 @@ impl Primitive for PlaneXY {
             None
         } else {
             let t = (pt!(0, 0, 0) - ray.origin).z() / d;
-            let p = ray.at(t);
-            let local_position = LocalPosition {
-                xyz: p,
-                uv: compute_uv_coordinates(p),
-            };
-            let coordinate_system = compute_coordinate_system(p);
-            let material = Rc::new(UniformMaterial::new(Color::black()));
-            let hit = Hit {
-                material,
-                t,
-                local_position,
-                transformation: coordinate_system.as_transformation(),
-            };
 
-            Some(hit)
+                if t > 0.0 {
+                let p = ray.at(t);
+                let local_position = LocalPosition {
+                    xyz: p,
+                    uv: compute_uv_coordinates(p),
+                };
+                let coordinate_system = compute_coordinate_system(p, &ray.origin);
+                let material = Rc::new(UniformMaterial::new(Color::black()));
+                let hit = Hit {
+                    material,
+                    t,
+                    local_position,
+                    transformation: coordinate_system.as_transformation(),
+                };
+
+                Some(hit)
+            } else {
+                None
+            }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use rstest::rstest;
+
+    use crate::math::approx;
+
+    #[cfg(test)]
+    use super::*;
+
+    #[rstest]
+    fn plane_hit_1() {
+        let plane = PlaneXY::new();
+        let ray = {
+            let origin = pt!(0, 0, 5);
+            let direction = vc!(0, 0, -1);
+            Ray::new(origin, direction)
+        };
+        let hit = plane.find_first_positive_hit(&ray).unwrap();
+
+        assert_eq!(approx(pt!(0, 0, 0)), hit.global_position());
+        assert_eq!(approx(vc!(0, 0, 1)), hit.normal());
+    }
+
+    #[rstest]
+    fn plane_hit_2() {
+        let plane = PlaneXY::new();
+        let ray = {
+            let origin = pt!(0, 0, -5);
+            let direction = vc!(0, 0, 1);
+            Ray::new(origin, direction)
+        };
+        let hit = plane.find_first_positive_hit(&ray).unwrap();
+
+        assert_eq!(approx(pt!(0, 0, 0)), hit.global_position());
+        assert_eq!(approx(vc!(0, 0, -1)), hit.normal());
     }
 }
